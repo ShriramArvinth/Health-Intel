a401be8903dd848282f33cf001c37e75fb7d921e
Shriram-Icliniq <shriram.a@icliniq.com>
2025-02-14 22:03:57 +0530

    configuring logging according to environment | Shriram Arvinth

--- app/error_logger.py
+++ app/error_logger.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 import logging
 from enum import Enum
 import traceback
+from google.cloud import logging as cloud_logging
+import os
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -32,12 +34,32 @@ def log_error(error: Error, severity: Severity):
     stack_trace = ''.join(traceback.format_exception(None, error.excpetion, error.excpetion.__traceback__))
     log_message = f"module={error.module} code={error.code} description=\"{error.description}\" stack_trace=\"{stack_trace}\""
 
-    if severity == Severity.INFO:
-        logger.info(log_message)
-    elif severity == Severity.WARN:
-        logger.warning(log_message)
-    elif severity == Severity.ERROR:
-        logger.error(log_message)
-    elif severity == Severity.CRITICAL:
-        logger.critical(log_message)
-                
+
+    if is_running_in_gcp():
+        print("Running in GCP")
+        # Google Cloud Logging
+        client = cloud_logging.Client()
+        cloud_logger = client.logger(error.module)
+
+        if severity == Severity.INFO:
+            cloud_logger.log_text(log_message, severity='INFO')
+        elif severity == Severity.WARN:
+            cloud_logger.log_text(log_message, severity='WARNING')
+        elif severity == Severity.ERROR:
+            cloud_logger.log_text(log_message, severity='ERROR')
+        elif severity == Severity.CRITICAL:
+            cloud_logger.log_text(log_message, severity='CRITICAL')
+    else:
+        print("Running in Non-GCP")
+        # Console Logging
+        if severity == Severity.INFO:
+            logger.info(log_message)
+        elif severity == Severity.WARN:
+            logger.warning(log_message)
+        elif severity == Severity.ERROR:
+            logger.error(log_message)
+        elif severity == Severity.CRITICAL:
+            logger.critical(log_message)
+
+def is_running_in_gcp():
+    return os.getenv('GAE_ENV', '').startswith('standard') or os.getenv('K_SERVICE') is not None
