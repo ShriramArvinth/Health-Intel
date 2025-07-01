81d91142d2737a0d269383b03b22402b0d5e1ab5
Shriram-Icliniq <shriram.a@icliniq.com>
2025-07-01 15:50:15 +0530

    change data formatting approach | Shriram Arvinth

--- data_prep/prep_induvidual_datasources/ebook_prep/ebook_to_xml.py
+++ data_prep/prep_induvidual_datasources/ebook_prep/ebook_to_xml.py
@@ -1,78 +1,29 @@
 import re
-from typing import List
-import dicttoxml2
-import html
-import json
-from lxml import etree
-
-class Document:
-    def __init__(self, content: str = ""):
-        self.content = content
-
-    def to_dict(self):
-        return {
-            "content": self.content
-        }
-
-class JSONEbookTemplate:
-    def __init__(self, all_chapters: List[Document]):
-        self.all_chapters = all_chapters
-
-    def to_dict(self):
-        return {
-            "all_chapters": {
-                "item_name_chapter": [document.to_dict() for document in self.all_chapters]
-            }
-        }
 
 file_path = './knowledge.txt'
 with open(file_path, 'r') as file:
     content = file.read()
 
-# replace >, < with their textual versions, as it will throw erros when converted to XML
+# Replace < and > with textual versions to avoid XML issues
 content = re.sub(r"<", "less than", content)
 content = re.sub(r">", "greater than", content)
+# Replace curly apostrophes with straight ones
+content = re.sub(r"’", "'", content)
 
-# replace unrecognizable characters with proper characters
-content = re.sub(r"’", "\'", content)
-content = re.sub(r"", "", content)
+# Split content into sections as before
+sections = [m.group() for m in re.finditer(r'((?:^Section \d+.*?(?=^Section \d+|\Z))|(?:^.+?(?=^Section \d+|\Z)))', content, re.MULTILINE | re.DOTALL)]
 
-ebook = JSONEbookTemplate(
-    all_chapters = []
-)
+# Build XML string
+xml_lines = ["<all_chapters>"]
+for section in sections:
+    xml_lines.append("  <chapter>")
+    xml_lines.append("    <content>")
+    xml_lines.append(section.strip())
+    xml_lines.append("    </content>")
+    xml_lines.append("  </chapter>")
+xml_lines.append("</all_chapters>")
 
-for section_match in re.finditer(r'((?:^Section \d+.*?(?=^Section \d+|\Z))|(?:^.+?(?=^Section \d+|\Z)))', content, re.MULTILINE | re.DOTALL):
-    ebook.all_chapters.append(
-        Document(
-            content = section_match.group()
-        )
-    )
+xml_string = "\n".join(xml_lines)
 
-# with open(f"./ebook.json", "w") as file:
-#     file.write(json.dumps(ebook.to_dict()))
-
-def item_func(x):
-    return (x.split("_")[-1])
-ebook_xml = dicttoxml2.dicttoxml(
-    obj = ebook.to_dict(), 
-    root = False, 
-    attr_type = False,
-    item_func = item_func
-)
-
-# xml to proper string from a bytes object
-    # html.unescape converts elements that are unknown to utf-8 to their proper string format in unicode
-proper_string = str(html.unescape(ebook_xml.decode('utf-8', errors="ignore")))
-
-# # write uncleaned xml to file
-# with open("./ebook1.xml", "w") as file:
-#      file.write(proper_string)
-
-# cleaning the XML string
-fragment = etree.fromstring(proper_string)
-etree.strip_tags(fragment,'item_name_chapter')
-xml_tree_string = etree.tostring(fragment, pretty_print = True)
-
-# write to file in proper utf-8 characters
-with open("./ebook1.xml", "w") as file:
-     file.write(html.unescape(xml_tree_string.decode('utf-8', errors="ignore")))
+with open("./ebook1.xml", "w", encoding="utf-8") as f:
+    f.write(xml_string)
